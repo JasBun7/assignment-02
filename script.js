5b00b843ddebc416ac7d8141a255391b158628a2
JasBun7 <Jasonzys365@gmail.com>
2026-03-05 17:32:09 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-console.log("JavaScript is connected!");
-
-const form = document.querySelector("form");
-
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Thanks for your interest in NorthStar Study Club!");
-});
