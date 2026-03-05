b1f39124df80c27cf0d5a2d574907ecd3adee551
JasBun7 <Jasonzys365@gmail.com>
2026-03-05 17:15:19 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log("JavaScript is connected!");
+
+const joinButton = document.querySelector(".btn");
+
+joinButton.addEventListener("click", function() {
+    alert("Thanks for your interest in NorthStar Study Club!");
+});
